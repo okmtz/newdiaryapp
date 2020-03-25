4b2de45ff5c942fbb3698853e5f79d89d9a14d94
souken-b <yuto430scuderia@gmail.com>
2020-03-25 16:39:46 +0900

    make table contents,posts,memos

---
+++ database/migrations/2020_03_25_071555_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("posts", function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->integer('content_id');
+            $table->string('title')->nullable;
+            $table->string('content')->nullable;
+            $table->timestamps();
+        });
+
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
