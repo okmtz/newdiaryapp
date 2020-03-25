78c61c9934d56c823acef759be6d02d4627c5e30
souken-b <yuto430scuderia@gmail.com>
2020-03-25 18:49:32 +0900

    change redirect route after login to contents_show

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ContentController extends Controller
 {    
-    
+    public function index()
+    {
+        $user = Auth::user();
+
+        return view('content_show', compact('user'));
+    }
 
     //
 }
