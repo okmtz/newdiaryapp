6ceb22c327778ea29071587d6e7e08f46d95430d
souken-b <yuto430scuderia@gmail.com>
2020-03-29 17:15:56 +0900

    add css to resources/views/posts/show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,3 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>posts contents</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <h5 class="text-align-left mt-3 ml-3">{{ $post=id }}</h3>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto mr-3">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ action('PostController@index', ['id' => $content_id]) }}">Post Top</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ action('ContentController@index') }}">Top Page </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    </body>
+</html>
+
 <a href="{{ action('PostController@edit', $post) }}">投稿を編集する</a>
 <a href="{{ action('PostController@index', ['id' => $post->content_id]) }}">投稿の一覧に戻る</a>
 <form method="POST" action="{{action('PostController@destroy', $post)}}">
