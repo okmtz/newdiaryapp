9a447c0995b43dc907d2309c8525cb901cca29b7
souken-b <yuto430scuderia@gmail.com>
2020-03-30 00:04:45 +0900

    add css to resources/views/posts/show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -21,43 +21,79 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <h5 class="text-align-left mt-3 ml-3">{{ $post=id }}</h3>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto mr-3">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ action('PostController@index', ['id' => $content_id]) }}">Post Top</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ action('ContentController@index') }}">Top Page </a>
-                    </li>
-                </ul>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <h5 class="text-align-left mt-3 ml-3">{{ $post->id }}</h3>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto mr-3">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ action('PostController@index', ['id' => $post->content_id]) }}">Post Top</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ action('ContentController@index') }}">Top Page </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <div class="container mb-5">
+            <div class="row-cols-1 mt-5">
+                <div class="btn-group mb-3" role="group">
+                    <form method="POST" action="{{ action('PostController@edit', $post) }}">
+                        @csrf 
+                        @method('GET')
+                        <button class="btn btn-default mr-2">Edit</button>
+                    </form>
+                    <form method="POST" action="{{action('PostController@destroy', $post)}}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-default mr-2">delete</button>
+                    </form>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <span class="text-left"><h4>{{ $post->title }}</h4></span>
+                        <div class="text-right">
+                            <span class="mr-2">{{ "last modified " .$post->updated_at->format('Y年m月d日H時i分') }}</span>
+                            <span>{{ "upload " . $post->created_at->format('Y年m月d日H時i分') }}</span>
+                        </div>
+                    </div>
+                    <div class="card-body text-center">{{ $post->content }}</div>
+                </div>
             </div>
-        </nav>
-    </header>
+            <div class="row row-cols-2 mt-5">
+                <div class="accordion col-7" id="accordionExample">
+                    @foreach($memos as $memo)
+                        <div class="card">
+                            <div class="card-header" id="{{ 'heading' . $memo->id }}">
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="{{ '#collapse' . $memo->id }}" aria-controls="{{ 'collapse' .$memo->id }}">
+                                        @php
+                                            $memo_content = Illuminate\Support\Str::limit($memo->content,25);
+                                        @endphp
+                                        {{ $memo_content }}
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="{{ 'collapse' . $memo->id }}"class="collapse" aria-labelledby="{{ 'heading' . $memo->id }}" data-parent="#accordionExample">
+                                <div class="card-body">
+                                    {{ $memo->content }}
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="card col-4 ml-3 px-0">
+                    <div class="card-body text-center px-1 py-1">
+                    <form method="POST" class="px-0 pt-0" action="{{action('MemoController@store')}}">
+                        @csrf
+                        <input type="hidden" name="post_id" value="{{$post->id}}">
+                        <textarea class="mx-0" style="min-width: 100%" rows=15 name="content" placeholder="メモを入力してください"></textarea>
+                        <br>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                </div>
+            </div>
     </body>
 </html>
-
-<a href="{{ action('PostController@edit', $post) }}">投稿を編集する</a>
-<a href="{{ action('PostController@index', ['id' => $post->content_id]) }}">投稿の一覧に戻る</a>
-<form method="POST" action="{{action('PostController@destroy', $post)}}">
-    @csrf
-    @method('DELETE')
-    <button type="submit">delete</button>
-</form>
-<p>title</p>
-<p>{{$post->title}}</p>
-<p>content</p>
-<p>{{$post->content}}</p>
-@foreach($memos as $memo)
-    <p>memo content</p>
-    <p>{{$memo->content}}</p>
-@endforeach
-<form method="POST" action="{{action('MemoController@store')}}">
-    @csrf
-    <input type="hidden" name="post_id" value="{{$post->id}}">
-    <textarea name="content" placeholder="メモを入力してください"></textarea>
-    <button type="submit">Submit</button>
-</form>
 
